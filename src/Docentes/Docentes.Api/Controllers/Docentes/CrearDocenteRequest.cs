7e54d4b208afd6f2662edeb504eeb453fd4a6057
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-09-24 20:39:41 -0500

    Se agrega [JsonRequired] en CrearDocenteRequest

--- src/Docentes/Docentes.Api/Controllers/Docentes/CrearDocenteRequest.cs
+++ src/Docentes/Docentes.Api/Controllers/Docentes/CrearDocenteRequest.cs
@@ -1,9 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace Docentes.api.Controllers.Docentes;
 
 public record CrearDocenteRequest 
-(
-    Guid UsuarioId,
-    Guid EspecialidadId,
-    string datoTemporal,
-    int datoTemporaInt
-);
+{
+    [JsonRequired] public Guid UsuarioId {get; init; }
+    [JsonRequired] public Guid EspecialidadId {get; init; }
+}
